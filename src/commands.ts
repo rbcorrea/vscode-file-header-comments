62c533192ca2b02e37b7f6f710224764730c66ce
animu5 <d.schwachhofer@mailbox.org>
2017-04-08 02:58:40 +0200

    initial commit

--- src/commands.ts
+++ src/commands.ts
@@ -1,15 +1,17 @@
 "use strict";
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
+var path = require('path');
+var dateFormat = require('dateformat');
 
-let configPrefix: String = "fileHeaderCommentHelper";
+let configPrefix: string = "fileHeaderCommentHelper";
 
 export function insertFileHeaderComment() {
     let _workspace = vscode.workspace;
-    let _window    = vscode.window;
-    let _editor    = _window.activeTextEditor;
-    let _root      = _workspace.rootPath;
+    let _window = vscode.window;
+    let _editor = _window.activeTextEditor;
+    let _root = _workspace.rootPath;
 
-    var projConf  = _workspace.getConfiguration((configPrefix + ".projectSettings"));
+    var projConf = _workspace.getConfiguration((configPrefix + ".projectSettings"));
     var langConfs = _workspace.getConfiguration((configPrefix + ".languageConfigs"));
 
     var values = {
@@ -19,25 +21,26 @@ export function insertFileHeaderComment() {
 
     if (_root !== undefined && _editor !== undefined) {
         var languageStr = ("language_" + _editor.document.languageId);
-        
+
         if (projConf.has("projectName") && projConf.get("projectName") !== null) {
             values.projectName = projConf.get("projectName");
         } else {
-            values.projectName = _root.substr(_root.lastIndexOf("/") + 1);
+            var folders = _root.split(path.sep)
+            values.projectName = folders[folders.length - 1];
         }
 
         values.currentFile = _editor.document.fileName.replace(_root, "").substr(1);
-        
+
         if (langConfs.has(languageStr)) {
-            var template = (langConfs.get(languageStr + ".template") as Array<String>).join("\n");
-            
+            var template = (langConfs.get(languageStr + ".template") as Array<string>).join("\n");
+
             _editor.edit((edit) => {
                 edit.insert(new vscode.Position(0, 0), template
                     .replace("$(projectName)", values.projectName)
                     .replace("$(currentFile)", values.currentFile)
                     .replace("$(date)", (new Date()).toLocaleString()));
             });
-            
+
             vscode.commands.executeCommand("workbench.action.files.save");
         } else {
             var openGlobalSettingsItem: vscode.MessageItem = {
@@ -46,10 +49,10 @@ export function insertFileHeaderComment() {
             var openWorkspaceSettingsItem: vscode.MessageItem = {
                 "title": "Open Workspace Settings"
             };
-            
+
             vscode.window.showErrorMessage(
                 ("Unable to locate file-header-comment template for " +
-                _editor.document.languageId + "."),
+                    _editor.document.languageId + "."),
                 openGlobalSettingsItem, openWorkspaceSettingsItem
             ).then((selectedItem: vscode.MessageItem) => {
                 if (selectedItem === openGlobalSettingsItem) {
